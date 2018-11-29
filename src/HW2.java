180ff55700bb6b634a8c2282351d1cce5c5d1799
viktor <victorchernyaew@gmail.com>
2018-11-29 11:32:51 +0400

    add pj

---
+++ src/HW2.java
@@ -0,0 +1,13 @@
+
+/**
+ * @version 29.11.2018
+ * @author Viktor Chernyaev
+ * @JavaAlgorithmsAndStructures homework Lesson-2
+ * @link https://github.com/des1z
+ */
+
+public class HW2 {
+    public static void main(String[] args) {
+        
+    }
+}
